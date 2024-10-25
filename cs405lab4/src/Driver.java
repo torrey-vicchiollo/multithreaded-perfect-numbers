4e4ad285511d1f858d14e34426655edc93e9cb14
torrey-vicchiolloe <torreyvicchiollo@outlook.com>
2024-10-25 13:24:13 -0500

    minor update

--- cs405lab4/src/Driver.java
+++ cs405lab4/src/Driver.java
@@ -11,16 +11,16 @@ public class Driver {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the upper bound (N): ");
+        System.out.print("ENTER UPPER BOUND (N) [1000000 <= N <= 1000000000] >> ");
         int N = scanner.nextInt();
         if ((N < 1000000) || (N > 1000000000)) {
-            System.err.println("Invalid input of N! Defaulting to 1,000,000");
+            System.err.println("INVALID INPUT OF N >> DEFAULTING TO 1,000,000");
             N = 1000000;
         }
-        System.out.println("Enter the number of threads (nThreads): ");
+        System.out.print("ENTER NUMBER OF THREADS (nThreads) [1 <= nThreads <= 10] >> ");
         int nThreads = scanner.nextInt();
         if ((nThreads < 1) || (nThreads > 10)) {
-            System.err.println("Invalid input of nThreads! Defaulting to 2");
+            System.err.println("INVALID INPUT OF nThreads >> DEFAULTING TO 2");
             nThreads = 2;
         }
         scanner.close();
@@ -30,21 +30,22 @@ public class Driver {
         int lowerBound = 1;
         for (int i = 1; i <= nThreads; i++) {
             Future<List<Integer>> result = executor.submit(new PerfectNumber(lowerBound, i == nThreads ? N : (N / nThreads) * i));
-            System.out.println("Started thread " + i + ", working from " + lowerBound + " to " + (N / nThreads) * i);
+            System.out.println("STARTED THREAD " + i + " FROM " + lowerBound + " TO " + (N / nThreads) * i);
             futureArr.add(result);
             lowerBound = ((N / nThreads) * i) + 1;
         }
+        System.out.print("PERFECT NUMBERS >> ");
         for (Future<List<Integer>> future : futureArr) {
             try {
                 List<Integer> result = future.get();
                 for (int i : result) {
-                    System.out.print(i + ", ");
+                    System.out.print(i + " ");
                 }
             } catch (InterruptedException | ExecutionException e) {
             }
         }
         long endTime = System.currentTimeMillis();
-        System.out.print("Elapsed time: " + (endTime - startTime) + " ms");
+        System.out.print("\nELAPSED TIME >> " + (endTime - startTime) + " ms");
         System.exit(-1);
     }
 }
--- cs405lab4/src/PerfectNumber.java
+++ cs405lab4/src/PerfectNumber.java
@@ -5,11 +5,11 @@ import java.util.concurrent.Callable;
 
 public class PerfectNumber implements Callable<List<Integer>> {
 
-    private int lowerBound, upperBound;
+    private final int lowerBound, upperBound;
 
-    public PerfectNumber(int lower, int upper) {
-        this.lowerBound = lower;
-        this.upperBound = upper;
+    public PerfectNumber(int l, int u) {
+        this.lowerBound = l;
+        this.upperBound = u;
     }
 
     @Override
@@ -19,11 +19,7 @@ public class PerfectNumber implements Callable<List<Integer>> {
             int sum = 1;
             for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
-                    if (j * j != i) {
-                        sum = sum + j + i / j;
-                    } else {
-                        sum = sum + j;
-                    }
+                    sum += j * j != i ? j + i / j : j;
                 }
             }
             if (sum == i && i != 1) {
